7ead4a3a59865d5abb5b69576d28d40f56c0386b
wuyuansushen <819070864@qq.com>
2021-04-16 20:37:09 +0800

    Add project files.

---
+++ TupleTutorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TupleTutorial
+{
+    class Program
+    {
+        static (int interger,double decimalPart) separateDouble(double PNumber)
+        {
+            double outDec = PNumber % 1;
+            int outInt = (int)(PNumber - outDec);
+
+            return (outInt, outDec);
+        }
+        static void Main(string[] args)
+        {
+            var outTest = separateDouble(113.514);
+            Console.WriteLine($"{outTest.interger}\n{outTest.decimalPart:F3}");
+        }
+    }
+}
